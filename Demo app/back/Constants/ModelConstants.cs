af4d8f071d6feebcb0d1764fec57c17df1943d72
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-10-23 17:33:16 +0200

    Dodata tabela Country #6, kreiran gitignore #8
    Креирана класа Country која представља шифарник држава, додат .gitignore фајл са неким од путања које се не посматрају

--- Demo app/back/Constants/ModelConstants.cs
+++ Demo app/back/Constants/ModelConstants.cs
@@ -2,7 +2,8 @@
 {
     public class ModelConstants
     {
-        public const int LENGTH_NAME     = 100;
-        public const int LENGTH_EMAIL    = 150;
+        public const int LENGTH_NAME       = 100;
+        public const int LENGTH_EMAIL      = 150;
+        public const int LENGTH_SHORT_NAME = 50; 
     }
 }
---
+++ Demo app/back/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace PyxisKapriBack.Models
+{
+    public class Country
+    {
+        [Key]
+        public long Id { get; set; }
+        [MaxLength(Constants.ModelConstants.LENGTH_SHORT_NAME)]
+        public string Name { get; set; }
+
+    }
+}
