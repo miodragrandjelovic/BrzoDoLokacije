97b98ba742e3357c1fa480ebc899c456fd62db2c
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-10-29 17:50:11 +0200

    FileService #13
    Dodate funkcije za konverziju slike u niz bajtova i obrnuto ukoliko bude zatrebalo

--- ProductionApp/back/Services/FileService.cs
+++ ProductionApp/back/Services/FileService.cs
@@ -1,16 +1,22 @@
 ﻿using PyxisKapriBack.Services.Interfaces;
-
+using System.Drawing;
 namespace PyxisKapriBack.Services
 {
     public class FileService : IFileService
     {
-        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        public byte[] ImageToByteArray(Image image)
         {
-            using (var ms = new MemoryStream())
-            {
-                imageIn.Save(ms, imageIn.RawFormat);
-                return ms.ToArray();
-            }
+            MemoryStream ms = new MemoryStream(); 
+            image.Save(ms, image.RawFormat);
+            return ms.ToArray();
+        }
+        public Image ByteArrayToImage(byte[] byteArray)
+        {
+            MemoryStream ms = new MemoryStream(byteArray, 0, byteArray.Length);
+            ms.Write(byteArray, 0, byteArray.Length);
+            Image image = Image.FromStream(ms, true);
+
+            return image; 
         }
     }
 }
