6fe78c19458e67bf9d585fa4f14cd1279a705ebe
Miodrag Randjelovic <dobropamtimsve00@gmail.com>
2022-10-31 18:14:56 +0100

    odradjen deo novog dizajna

---
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/NewPostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pyxiskapri.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pyxiskapri.R
+
+class NewPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_post)
+    }
+}
