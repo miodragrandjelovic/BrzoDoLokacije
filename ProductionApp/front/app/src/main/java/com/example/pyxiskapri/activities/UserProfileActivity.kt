bee882713f6e1b3713286a43ee522b7ccdb3f35b
Miodrag Randjelovic <dobropamtimsve00@gmail.com>
2022-11-03 20:24:57 +0100

    Kreiran dizajn za stranicu userProfile

---
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pyxiskapri.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pyxiskapri.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
