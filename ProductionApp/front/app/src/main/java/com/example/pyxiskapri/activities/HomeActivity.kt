842639ff7f9897e3c9f7089f0bbcac815bbdb641
Ivan Djordjevic <dj.iwan00@gmail.com>
2022-11-04 20:51:23 +0100

    Primene dizajna sa figme kao i dodavanje dugmica za izbor kategorije prikazanih postova. #11

--- ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/HomeActivity.kt
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/HomeActivity.kt
@@ -1,19 +1,13 @@
 package com.example.pyxiskapri.activities
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pyxiskapri.R
 import com.example.pyxiskapri.adapters.PostListAdapter
-import com.example.pyxiskapri.dtos.response.MessageResponse
 import com.example.pyxiskapri.dtos.response.PostResponse
 import kotlinx.android.synthetic.main.activity_home.*
-import com.example.pyxiskapri.models.UserData
-import com.example.pyxiskapri.utility.ActivityControl
 import com.example.pyxiskapri.utility.ApiClient
 import com.example.pyxiskapri.utility.SessionManager
 import retrofit2.Call
@@ -40,8 +34,8 @@ class HomeActivity : AppCompatActivity() {
 
         fillPostsRV()
 
-        setupButtonHome()
-        setupButtonNotifications()
+        setupButtonNewPost()
+        setupButtonLogout()
     }
 
     override fun onRestart() {
@@ -49,16 +43,16 @@ class HomeActivity : AppCompatActivity() {
         fillPostsRV()
     }
 
-    private fun setupButtonNotifications(){
-        btn_notifications.setOnClickListener {
-            sessionManager.clearToken()
-            val intent = Intent (this, MainActivity::class.java);
-            startActivity(intent);
-        }
+    private fun setupButtonLogout(){
+//        btn_notifications.setOnClickListener {
+//            sessionManager.clearToken()
+//            val intent = Intent (this, MainActivity::class.java);
+//            startActivity(intent);
+//        }
     }
 
-    private fun setupButtonHome(){
-        btn_home.setOnClickListener {
+    private fun setupButtonNewPost(){
+        btn_newPost .setOnClickListener {
             val intent = Intent (this, NewPostActivity::class.java);
             startActivity(intent);
         }
