d9544cb66371aacc6378fb6bd4bec19ed662857b
Ivan Djordjevic <dj.iwan00@gmail.com>
2022-11-08 21:39:22 +0100

    Kreiran poziv i omogucen prikaz podataka o postu i izmenjene odredjene data klase. #32

--- ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/OpenPost.kt
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/OpenPost.kt
@@ -1,12 +1,79 @@
 package com.example.pyxiskapri.activities
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.pyxiskapri.R
+import com.example.pyxiskapri.dtos.response.PostFullResponse
+import com.example.pyxiskapri.dtos.response.PostResponse
+import com.example.pyxiskapri.utility.ApiClient
+import com.example.pyxiskapri.utility.SessionManager
+import com.example.pyxiskapri.utility.UtilityFunctions
+import kotlinx.android.synthetic.main.activity_open_post.*
+import kotlinx.android.synthetic.main.activity_open_post.view.*
+import kotlinx.android.synthetic.main.item_post.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class openPost : AppCompatActivity() {
+class OpenPost : AppCompatActivity() {
+    lateinit var sessionManager: SessionManager
+    lateinit var apiClient: ApiClient
+
+    private var postId: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open_post)
+        postId = savedInstanceState!!.getInt("postId")
+
+        sessionManager = SessionManager(this)
+        apiClient = ApiClient()
+
     }
+
+    private fun requestPostData(){
+        apiClient.getPostService(this).getPostById(postId)
+            .enqueue(object : Callback<PostFullResponse> {
+                override fun onResponse(call: Call<PostFullResponse>, response: Response<PostFullResponse>) {
+                    if(response.isSuccessful)
+                        updatePostData(response.body()!!)
+
+
+                }
+
+                override fun onFailure(call: Call<PostFullResponse>, t: Throwable) {
+
+                }
+
+            })
+    }
+
+    private fun updatePostData(postData: PostFullResponse){
+        iv_ownerAvatar.setImageBitmap(UtilityFunctions.base64ToBitmap(postData.ownerImage))
+
+        tv_ownerUsername.text = postData.ownerUsername
+        tv_postDescription.text = postData.postDescription
+
+        iv_ownerAvatar.setImageBitmap(UtilityFunctions.base64ToBitmap(postData.coverImage))
+
+        // ADDITIONAL IMAGES
+
+        tv_likeCount.text = postData.likeCount.toString()
+        tv_viewCount.text = postData.viewCount.toString()
+
+        // LIKE CHECK
+
+        // DISLIKE CHECK
+
+        // FOLLOW CHECK
+
+        // REPORT CHECK
+
+
+        // COMMENT SETUP
+
+    }
+
 }
--- ProductionApp/front/app/src/main/java/com/example/pyxiskapri/models/PostListItem.kt
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/models/PostListItem.kt
@@ -3,11 +3,13 @@ package com.example.pyxiskapri.models
 import com.example.pyxiskapri.dtos.response.PostResponse
 
 data class PostListItem(
-    var username: String,
-    var image: String,
+    var id: Int,
+    var ownerUsername: String,
+    var ownerImage: String,
+    var coverImage: String,
     var likeCount: Int,
     var viewCount: Int
 )
 {
-    constructor(response: PostResponse) : this("", response.image, response.likeCount, response.viewCount)
+    constructor(response: PostResponse) : this(response.id, response.ownerUsername, response.ownerImage, response.coverImage, response.likeCount, response.viewCount)
 }
