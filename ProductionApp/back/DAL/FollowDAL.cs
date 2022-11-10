d6616e35e469c5eeb317418a93fbf68f0a9f5e3a
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-11-10 21:42:11 +0100

    Dodate funkcije #45
    Dodate su dve funkcije - jedna vraća pratioce korisnika, a druga vraća korisnike koje je on zapratio

--- ProductionApp/back/DAL/FollowDAL.cs
+++ ProductionApp/back/DAL/FollowDAL.cs
@@ -1,8 +1,25 @@
 ﻿using PyxisKapriBack.DAL.Interfaces;
+using PyxisKapriBack.Models;
 
 namespace PyxisKapriBack.DAL
 {
     public class FollowDAL : IFollowDAL
     {
+        private Database _context; 
+        public FollowDAL(Database context)
+        {
+            _context = context;
+        }
+        // trazimo one naloge gde je zapracen ovaj user 
+        public List<Follow> GetFollowers(string username)
+        {
+            return _context.Follow.Where(follow => follow.Following.Username.Equals(username)).ToList();
+        }
+
+        // trazimo one naloge koji su zapraceni od ovog korisnika
+        public List<Follow> GetFollowing(string username)
+        {
+            return _context.Follow.Where(follow => follow.Follower.Username.Equals(username)).ToList();
+        }
     }
 }
--- ProductionApp/back/DAL/Interfaces/IFollowDAL.cs
+++ ProductionApp/back/DAL/Interfaces/IFollowDAL.cs
@@ -1,6 +1,10 @@
-﻿namespace PyxisKapriBack.DAL.Interfaces
+﻿using PyxisKapriBack.Models;
+
+namespace PyxisKapriBack.DAL.Interfaces
 {
     public interface IFollowDAL
     {
+        List<Follow> GetFollowers(string username); 
+        List<Follow> GetFollowing(string username); 
     }
 }
