99de503529794c266cb643ac062bb88e520acd93
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-11-12 16:55:55 +0100

    Ispravke #45
    Funkcije sada vraćaju listu User-a. Treba da se testira da li je ok.

--- ProductionApp/back/DAL/FollowDAL.cs
+++ ProductionApp/back/DAL/FollowDAL.cs
@@ -6,20 +6,36 @@ namespace PyxisKapriBack.DAL
     public class FollowDAL : IFollowDAL
     {
         private Database _context; 
-        public FollowDAL(Database context)
+        private IUserDAL _iUserDAL;
+        public FollowDAL(Database context, IUserDAL userDAL)
         {
             _context = context;
+            _iUserDAL = userDAL;
         }
         // trazimo one naloge gde je zapracen ovaj user 
-        public List<Follow> GetFollowers(string username)
+        public List<User> GetFollowers(string username)
         {
-            return _context.Follow.Where(follow => follow.Following.Username.Equals(username)).ToList();
+            User user = _iUserDAL.GetUser(username);
+            if (user == null)
+                return null;
+            return _context.Follow.Where(follow => follow.FollowingId == user.Id)
+                                                   .Include(follow => follow.Follower)
+                                                   .Select(follow => follow.Follower)
+                                                   .ToList();
+           // return _context.Follow.Where(follow => follow.Following.Username.Equals(username)).ToList();
         }
 
         // trazimo one naloge koji su zapraceni od ovog korisnika
-        public List<Follow> GetFollowing(string username)
+        public List<User> GetFollowing(string username)
         {
-            return _context.Follow.Where(follow => follow.Follower.Username.Equals(username)).ToList();
+            User user = _iUserDAL.GetUser(username);
+            if (user == null)
+                return null;
+            return _context.Follow.Where(follow => follow.FollowerId == user.Id)
+                                                   .Include(follow => follow.Following)
+                                                   .Select(follow => follow.Following)
+                                                   .ToList();
+            //return _context.Follow.Where(follow => follow.Follower.Username.Equals(username)).ToList();
         }
     }
 }
