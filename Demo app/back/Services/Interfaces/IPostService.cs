6b7f9ee95f3cd23b9e13c7d1f3b0e868cdc8d474
Stefan Janicijevic <stefan.janicijevic42@gmail.com>
2022-11-13 14:50:34 +0100

    Dodat websajt na git. #63

---
+++ Demo app/back/Services/Interfaces/IPostService.cs
@@ -0,0 +1,11 @@
+﻿namespace PyxisKapriBack.Services.Interfaces
+{
+    public interface IPostService
+    {
+        List<Post> GetUserPosts(int UserID);
+        List<Post> GetPostsForLocation(int LocationID);
+        void AddPost(Post post);
+        void DeletePost(Post post);
+        Post GetPost(int PostID);
+    }
+}
