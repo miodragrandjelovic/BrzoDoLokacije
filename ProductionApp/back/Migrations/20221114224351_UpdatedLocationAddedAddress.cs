feb86fd14dad92c67e56ee216568e6673e2a9eb6
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-11-14 23:44:30 +0100

    Proširena lokacija #65
    Dodato polje adresa

---
+++ ProductionApp/back/Migrations/20221114224351_UpdatedLocationAddedAddress.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PyxisKapriBack.Migrations
+{
+    public partial class UpdatedLocationAddedAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Locations",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Locations");
+        }
+    }
+}
--- ProductionApp/back/Models/Location.cs
+++ ProductionApp/back/Models/Location.cs
@@ -10,7 +10,9 @@
         public List<Post>? Posts { get; set; }
 
         public double Longitude { get; set; } = 0;
-        public double Latitude { get; set; } = 0; 
+        public double Latitude { get; set; } = 0;
+        [MaxLength(Constants.ModelConstants.LENGTH_DESCRIPTION)]
+        public string Address { get; set; } = string.Empty;
         #region 'City'
         public int CityID { get; set; }
         public City City { get; set; }
