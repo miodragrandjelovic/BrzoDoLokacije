f503e13a259cc671fbb18edad2883886bb52a73c
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-11-22 13:07:15 +0100

    Location Manager#27
    Greškom nije pušten LocationManager u sklopu prethodnog commit-a

--- ProductionApp/back/LocationManager/LocationManager.cs
+++ ProductionApp/back/LocationManager/LocationManager.cs
@@ -1,15 +1,32 @@
-﻿using PyxisKapriBack.LocationManager.Interfaces;
+﻿using PyxisKapriBack.Models;
 using System.Device.Location; 
 namespace PyxisKapriBack.LocationManager
 {
-    public class LocationManager : ILocationManager
+    public static class LocationManager
     {
-        public double GetDistance(double Latitude1, double Longitude1, double Latitude2, double Longitude2)
+        public static double GetDistance(double Latitude1, double Longitude1, double Latitude2, double Longitude2)
         {
             var coordinate1 = new GeoCoordinate(Latitude1, Longitude1); 
             var coordinate2 = new GeoCoordinate(Latitude2, Longitude2);
             
             return coordinate1.GetDistanceTo(coordinate2);
-        } 
+        }
+
+        public static List<Location> GetAllAroundLocations(Location location, List<Location> locations, double distance = Constants.Constants.DISTANCE)
+        {
+            var closest = new List<Location>();
+            double dist; 
+
+            foreach(Location loc in locations)
+            {
+                dist = GetDistance(location.Latitude, location.Longitude, loc.Latitude, loc.Longitude); 
+                if (dist <= distance)
+                {
+                    loc.Distance = dist; 
+                    closest.Add(loc);
+                }
+            }
+            return closest;
+        }
     }
 }
