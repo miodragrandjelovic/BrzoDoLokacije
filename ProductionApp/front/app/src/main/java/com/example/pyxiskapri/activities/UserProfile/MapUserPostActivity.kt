982e3422da199ddefcfbd78e48dcf686412aa40a
Miodrag Randjelovic <dobropamtimsve00@gmail.com>
2022-11-27 19:16:59 +0100

    implementiran getUser zahtev

--- ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/UserProfile/MapUserPostActivity.kt
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/UserProfile/MapUserPostActivity.kt
@@ -1,13 +1,18 @@
 package com.example.pyxiskapri.activities.UserProfile
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.PopupWindow
+import android.widget.Toast
 import com.example.pyxiskapri.R
 import com.example.pyxiskapri.activities.MainActivity
+import com.example.pyxiskapri.dtos.response.GetUserResponse
+import com.example.pyxiskapri.utility.ApiClient
 import com.example.pyxiskapri.utility.SessionManager
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,12 +21,18 @@ import kotlinx.android.synthetic.main.activity_map_user_post.*
 import kotlinx.android.synthetic.main.activity_new_user_profile.*
 import kotlinx.android.synthetic.main.activity_new_user_profile.menu_btn
 import kotlinx.android.synthetic.main.popup_menu.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.*
 
 class MapUserPostActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var geocoder: Geocoder
+
+    lateinit var apiClient: ApiClient
+    lateinit var sessionManager: SessionManager
 
     /////////////////////////////////
 
@@ -35,6 +46,12 @@ class MapUserPostActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_map_user_post)
 
         setupMap()
+
+        apiClient=ApiClient()
+        sessionManager= SessionManager(this)
+
+        setupGetUser()
+
         /////////////////
 
 
@@ -60,6 +77,43 @@ class MapUserPostActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }
     }
+
+
+
+    private fun setupGetUser() {
+
+        var context: Context =this
+
+        apiClient.getUserService(context).getUser()
+            .enqueue(object : Callback<GetUserResponse> {
+                override fun onResponse(
+                    call: Call<GetUserResponse>,
+                    response: Response<GetUserResponse>
+                ) {
+                    if(response.isSuccessful)
+                    {
+                        tv_name1_m.text=response.body()!!.firstName
+                        tv_name2_m.text=response.body()!!.lastName
+
+
+                        val picture=response.body()!!.profileImage
+                        if(picture!=null)
+                        {
+                            var imageData = android.util.Base64.decode(picture, android.util.Base64.DEFAULT)
+                            shapeableImageView_m.setImageBitmap(BitmapFactory.decodeByteArray(imageData, 0, imageData.size))
+                        }
+
+
+                    }
+                }
+
+                override fun onFailure(call: Call<GetUserResponse>, t: Throwable) {
+                    Toast.makeText(context,"Something went wrong!", Toast.LENGTH_LONG).show()
+                }
+
+            })
+    }
+
 
     private fun popup() {
         menu_btn.setOnClickListener() {
