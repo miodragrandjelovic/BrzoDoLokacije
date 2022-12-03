693fe2ceabfa4b8f90f17a581f8fd6e0e885ce83
Stefan Janicijevic <stefan.janicijevic42@gmail.com>
2022-12-03 22:17:01 +0100

    Dodat pyton service. Povezan sa API-jem. Napravljena get metoda kako bi se proverilo da li sve radi kako treba. #113

---
+++ ProductionApp/back/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PyxisKapriBack.PythonService;
+
+namespace PyxisKapriBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ServiceClient client;
+
+        public TestController(ServiceClient client)
+        {
+            this.client = client;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = client.GetMean().Result;
+            var message = new
+            {
+                message = response
+            };
+            return Ok(message);
+        }
+    }
+}
---
+++ ProductionApp/back/PythonService/ServiceClient.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace PyxisKapriBack.PythonService
+{
+    public class ServiceClient
+    {
+        private readonly HttpClient client;
+        private readonly IConfiguration configuration;
+
+        private readonly string uri;
+
+        public ServiceClient(HttpClient client, IConfiguration configuration)
+        {
+            this.client = client;
+            this.configuration = configuration;
+
+            //uri = configuration.GetSection("ML_Server_Config:http") + configuration.GetSection("ML_Server_Config:host").Value.ToString() + ":" + configuration.GetSection("ML_Server_Config:port").Value.ToString() + "/";
+        }
+
+        public async Task<String> GetMean()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://127.0.0.1:8000/");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(content);
+                return JsonConvert.DeserializeObject<string>(content);
+            }
+        }
+
+
+
+    }
+}
