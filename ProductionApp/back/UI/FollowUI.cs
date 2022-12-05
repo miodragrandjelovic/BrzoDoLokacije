deba803b13d20583af91cc229f5a94ff94fcb6cf
Jefimija Stamenovic <jefimija.stamenovic@gmail.com>
2022-12-05 10:06:40 +0100

    #120
    Promenjena je logika GetFollowers i GetFollowing - pozivaju funkciju createListUserShortDTO koja za vrednost ProfileImage setuje putanju do profilne slike

--- ProductionApp/back/UI/FollowUI.cs
+++ ProductionApp/back/UI/FollowUI.cs
@@ -27,40 +27,12 @@ namespace PyxisKapriBack.UI
 
         public List<UserShortDTO>? GetFollowers()
         {
-            var followers = _iFollowService.GetFollowers(_iUserService.GetLoggedUser());
-
-            var followersDTO = new List<UserShortDTO>();
-
-            foreach (var follow in followers)
-            {
-                followersDTO.Add(new UserShortDTO
-                {
-                    ProfileImage = follow.ProfileImage == null ? string.Empty : Convert.ToBase64String(follow.ProfileImage),
-                    Username = follow.Username,
-                    FirstName = follow.FirstName,
-                    LastName = follow.LastName
-                });
-            }
-            return followersDTO;
+            return createListUserShortDTO(_iFollowService.GetFollowers(_iUserService.GetLoggedUser()));
         }
 
         public List<UserShortDTO>? GetFollowing()
         {
-            var followings = _iFollowService.GetFollowing(_iUserService.GetLoggedUser());
-
-            var followingDTO = new List<UserShortDTO>();
-
-            foreach (var follow in followings)
-            {
-                followingDTO.Add(new UserShortDTO
-                {
-                    ProfileImage = follow.ProfileImage == null ? string.Empty : Convert.ToBase64String(follow.ProfileImage),
-                    Username = follow.Username,
-                    FirstName = follow.FirstName,
-                    LastName = follow.LastName
-                });
-            }
-            return followingDTO;
+            return createListUserShortDTO(_iFollowService.GetFollowing(_iUserService.GetLoggedUser()));
         }
 
         public Response IsFollowed(string followingUsername)
