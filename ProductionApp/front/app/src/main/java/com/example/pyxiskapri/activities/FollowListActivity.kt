f29bd65dbd132cdba11ccb803385bc9c1edfe204
Miodrag Randjelovic <dobropamtimsve00@gmail.com>
2022-12-08 02:23:24 +0100

    kreiran dizajn following/followers stranice

---
+++ ProductionApp/front/app/src/main/java/com/example/pyxiskapri/activities/FollowListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pyxiskapri.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pyxiskapri.R
+
+class FollowListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follow_list)
+    }
+}
